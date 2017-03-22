c455951b9a139731e1a6b8b1ca2cca2dda9f9d7b
Tonin <2670692@qq.com>
2017-03-22 08:41:15 +0800

    modify app.py

--- www/static/app.py
+++ www/static/app.py
@@ -1,11 +1,12 @@
-import logging;logging.basicConfig(level=logging.INFO)
+import logging
+logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
 
 @asyncio.coroutine
